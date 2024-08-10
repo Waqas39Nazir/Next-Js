2550975e425956108155687885a06b4d4a53faa8
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-10 17:42:15 +0500

    Loading Page

--- project-02/src/app/loading.tsx
+++ project-02/src/app/loading.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading TEST TEST TEST TEST TEST</h1>
+    <div className="flex flex-row w-full h-full items-center justify-center p-10 rounded-lg bg-green-500">
+      <h1>Loading..............</h1>
     </div>
+    // <div>
+    //   <h1>Loading TEST TEST TEST TEST TEST</h1>
+    // </div>
   );
 };
 
 export default Loading;
+
+// const delay = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Done");
+//     }, 3000);
+//   });
+// };
--- project-02/src/app/page-one/page.tsx
+++ project-02/src/app/page-one/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1> {data.name}</h1>
+      <h1>I am Your Page One</h1>
+      {/* <h1> {data.name}</h1> */}
     </div>
   );
 };

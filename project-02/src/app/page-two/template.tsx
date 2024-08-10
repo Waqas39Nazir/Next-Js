d6a5103c0a95e4578d438f091dd5f143a2e81411
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-10 15:50:16 +0500

    Layout and Templates

---
+++ project-02/src/app/page-two/template.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const Lemplate = () => {
+  console.log("I am Template please note down me");
+
+  return (
+    <div>
+      <h1>I am the Template lets test me baby</h1>
+    </div>
+  );
+};
+
+export default Lemplate;

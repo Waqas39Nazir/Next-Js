016c461a0260579b72414bb3020f95b6e18f2b24
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-10 16:12:13 +0500

    Layout and Templates

--- project-02/src/app/page-two/template.tsx
+++ project-02/src/app/page-two/template.tsx
@@ -1,12 +1,17 @@
 "use client";
 import React from "react";
 
-const Lemplate = () => {
+const Lemplate = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   console.log("I am Template please note down me");
 
   return (
     <div>
       <h1>I am the Template lets test me</h1>
+      {children}
     </div>
   );
 };

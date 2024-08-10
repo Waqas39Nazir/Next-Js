a4e679da46cc1bddd666adce2a87c15e7f19b07b
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-10 14:35:14 +0500

    Dynamic and Static Routes

---
+++ project-02/src/app/page-one/[productId]/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+
+const page = () => {
+  const { productId } = useParams();
+
+  console.log("Params:", productId);
+
+  return (
+    <div>
+      <h1>I am The Nested Static Page: {productId} </h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ project-02/src/app/page-one/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1> I am Your Page One</h1>
+    </div>
+  );
+};
+
+export default page;
